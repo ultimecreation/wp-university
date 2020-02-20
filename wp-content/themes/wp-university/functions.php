746e984842c603002a0a4d0a1befa6e5617d4de7
ultimecreation <naserbechata@gmail.com>
2020-02-20 11:34:50 +0100

    sass added to the css config

--- wp-content/themes/wp-university/functions.php
+++ wp-content/themes/wp-university/functions.php
@@ -1,4 +1,20 @@
 <?php
 
-// add theme support
-add_theme_support('post-thumbnails');
+
+
+// wpu theme setup
+add_action('after_setup_theme', 'wpu_setup_theme');
+function wpu_setup_theme()
+{
+    // enqueue styles
+    wp_enqueue_style('main-css', get_stylesheet_uri());
+
+    //enqueue scripts
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', array(), 'v1', true);
+
+
+    // add theme support
+    add_theme_support('post-thumbnails');
+
+    // en
+}
--- wp-content/themes/wp-university/home.php
+++ wp-content/themes/wp-university/home.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
 
-<h1>Index</h1>
+<h1>home</h1>
 <?php get_footer(); ?>
