adade667fad5846b50f85ea199bf4ef5c125169d
ultimecreation <naserbechata@gmail.com>
2020-02-21 18:22:18 +0100

    event section on front page done

--- wp-content/themes/wp-university/front-page.php
+++ wp-content/themes/wp-university/front-page.php
@@ -2,61 +2,80 @@
 
 <main id="front-page">
     <section id="header">
-        <div class="container">
-            <figure>
-                <img src=<?php echo get_template_directory_uri() . "/assets/img/header-1.jpg"; ?> alt="">
-                <figcaption>
-                    <div class="slogan">
-                        <h2>Notre Travail.</h2>
-                        <h2>Votre Réussite.</h2>
-                    </div>
-                </figcaption>
-            </figure>
-        </div>
+
+        <figure>
+            <img src=<?php echo get_template_directory_uri() . "/assets/img/header-1.jpg"; ?> alt="">
+            <figcaption>
+                <div class="slogan">
+                    <h2>Notre Travail.</h2>
+                    <h2>Votre Réussite.</h2>
+                </div>
+            </figcaption>
+        </figure>
+
     </section>
+    <!--EVENT SECTION-->
     <?php
     $events = new WP_Query(array(
         'post_type' => 'event',
-        'posts_per_page' => 2
+        'posts_per_page' => 2,
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'value' => (new Datetime('now'))->format('Y-m-d'),
+                'compare' => '>='
+            )
+        )
     ));
     //echo "<pre>" . print_r(get_post_meta(get_the_ID(), 'event_date'), true) . "</pre>"; 
     ?>
     <?php if ($events->found_posts > 0) : ?>
         <section id="events">
-            <header>
-                <h1>Événements</h1>
-            </header>
-            <?php while ($events->have_posts()) : $events->the_post(); ?>
-                <article class="event-post">
-                    <div class="card-header">
-                        <?php $date = get_post_meta(get_the_ID(), 'event_date', true);
-                        $date = new \DateTime($date);
-                        echo $date;
-                        ?>
+            <div class="container">
+                <header>
+                    <h1>Événements</h1>
+                </header>
+                <?php while ($events->have_posts()) : $events->the_post(); ?>
+                    <article class="event-post">
+                        <figure>
 
-                        <p><?php echo get_date_from_gmt(get_post_meta(get_the_ID(), 'event_date', true), 'H:i'); ?></p>
+                            <div class="event-img">
+                                <?php the_post_thumbnail();  ?>
+                                <figcaption>
+                                    <div class="card-date">
+                                        <?php $date = get_post_meta(get_the_ID(), 'event_date', true);
+                                        $day = date_i18n('d M', strtotime($date));
+                                        $year = date_i18n('Y', strtotime($date));
+                                        $hour = get_post_meta(get_the_ID(), 'event_hour', true);
+                                        echo "<p>$day</p><p>$year</p><p>$hour</p>";
+                                        ?>
+                                    </div>
+                                    <div class="card-body">
+                                        <h3><?php the_title(); ?></h3>
+                                        <p><?php the_excerpt(); ?></p>
+                                    </div>
+                                </figcaption>
+                            </div>
 
-                    </div>
-                    <div class="card-body">
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php the_excerpt(); ?></p>
-                    </div>
-                </article>
 
-            <?php endwhile; ?>
-
-
-
+                        </figure>
+                    </article>
+                <?php endwhile; ?>
+            </div>
         </section>
-    <?php else : ?>
-        <p></p>
 
     <?php endif; ?>
+    <!--EVENT SECTION END-->
+    <!--CAMPUS SECTION-->
+    <?php
+
+    ?>
     <section id="campus">
         <header>
             <h1>Campus</h1>
         </header>
     </section>
+    <!--CAMPUS SECTION END-->
     <section id="professors">
         <header>
             <h1>Professeurs</h1>
