6ad5622e622d028c7c1fc06982bcf7051467f747
ultimecreation <naserbechata@gmail.com>
2020-02-24 17:11:05 +0100

    front page completed and media queries completed

--- wp-content/themes/wp-university/footer.php
+++ wp-content/themes/wp-university/footer.php
@@ -1,6 +1,7 @@
 <footer>
     <?php wp_footer();
     ?>
+    <p>&copy; 2020</p>
 </footer>
 </body>
 
--- wp-content/themes/wp-university/front-page.php
+++ wp-content/themes/wp-university/front-page.php
@@ -2,16 +2,15 @@
 
 <main id="front-page">
     <section id="header">
+        <div class="overlay">
 
-        <figure>
-            <img src=<?php echo get_template_directory_uri() . "/assets/img/header-1.jpg"; ?> alt="">
-            <figcaption>
-                <div class="slogan">
-                    <h2>Notre Travail.</h2>
-                    <h2>Votre Réussite.</h2>
-                </div>
-            </figcaption>
-        </figure>
+        </div>
+        <div class="slogan">
+            <h1>Notre Travail.</h1>
+            <h1>Votre Réussite.</h1>
+
+        </div>
+
 
     </section>
     <!--EVENT SECTION-->
@@ -35,32 +34,37 @@
                 <header>
                     <h1>Événements</h1>
                 </header>
-                <?php while ($events->have_posts()) : $events->the_post(); ?>
-                    <article class="event-post">
-                        <figure>
+                <div class="event-post">
+                    <?php while ($events->have_posts()) : $events->the_post(); ?>
 
-                            <div class="event-img">
-                                <?php the_post_thumbnail();  ?>
-                                <figcaption>
-                                    <div class="card-date">
-                                        <?php $date = get_post_meta(get_the_ID(), 'event_date', true);
-                                        $day = date_i18n('d M', strtotime($date));
-                                        $year = date_i18n('Y', strtotime($date));
-                                        $hour = get_post_meta(get_the_ID(), 'event_hour', true);
-                                        echo "<p>$day</p><p>$year</p><p>$hour</p>";
-                                        ?>
-                                    </div>
-                                    <div class="card-body">
-                                        <h3><?php the_title(); ?></h3>
-                                        <p><?php the_excerpt(); ?></p>
-                                    </div>
-                                </figcaption>
-                            </div>
+                        <article>
+                            <figure>
+
+                                <div class="event-img">
+                                    <?php the_post_thumbnail();  ?>
+                                    <figcaption>
+                                        <div class="card-date">
+                                            <?php $date = get_post_meta(get_the_ID(), 'event_date', true);
+                                            $day = date_i18n('d M', strtotime($date));
+                                            $year = date_i18n('Y', strtotime($date));
+                                            $hour = get_post_meta(get_the_ID(), 'event_hour', true);
+                                            echo "<p>$day $year</p><p>$hour</p>";
+                                            ?>
+                                        </div>
+                                        <div class="card-body">
+                                            <h3><?php the_title(); ?></h3>
+                                            <p><?php the_excerpt(); ?></p>
+                                        </div>
+                                    </figcaption>
+                                </div>
 
 
-                        </figure>
-                    </article>
-                <?php endwhile; ?>
+                            </figure>
+                        </article>
+
+
+                    <?php endwhile; ?>
+                </div>
             </div>
         </section>
 
@@ -85,20 +89,24 @@
                     <h1>Campus</h1>
                 </header>
 
-                <?php while ($campus->have_posts()) : $campus->the_post(); ?>
-                    <article class="campus-post">
-                        <figure>
-                            <div class="campus-img">
-                                <?php the_post_thumbnail(); ?>
-                                <figcaption>
-                                    <h3> <?php the_title(); ?></h3>
-                                    <p><?php the_excerpt(); ?></p>
-                                </figcaption>
-                            </div>
+                <div class="campus-post">
+                    <?php while ($campus->have_posts()) : $campus->the_post(); ?>
+                        <article>
+                            <figure>
+                                <div class="campus-img">
+                                    <?php the_post_thumbnail(); ?>
+                                    <figcaption>
+                                        <h3> <?php the_title(); ?></h3>
+                                        <div class="excerpt">
+                                            <?php the_excerpt(); ?>
+                                        </div>
+                                    </figcaption>
+                                </div>
 
-                        </figure>
-                    </article>
-                <?php endwhile; ?>
+                            </figure>
+                        </article>
+                    <?php endwhile; ?>
+                </div>
             </div>
         </section>
     <?php endif; ?>
@@ -119,31 +127,34 @@
                     <h1>Programmes</h1>
                 </header>
 
-                <?php while ($programs->have_posts()) : $programs->the_post(); ?>
-                    <article class="program-post">
+                <div class="program-post">
+                    <?php while ($programs->have_posts()) : $programs->the_post(); ?>
+                        <article>
 
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php the_excerpt(); ?></p>
-                        <div class="related-campus">
-                            <?php
-                            global $wpdb;
-                            $related_campus = $wpdb->get_results(
-                                $wpdb->prepare("
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_excerpt(); ?></p>
+                            <div class="related-campus">
+                                <?php
+                                global $wpdb;
+                                $related_campus = $wpdb->get_results(
+                                    $wpdb->prepare("
                             SELECT *  FROM {$wpdb->prefix}campus_programs as campus_programs
                             JOIN {$wpdb->prefix}posts as campus
                             ON campus_programs.campus_id = campus.ID
                             WHERE campus_programs.program_id = '%d'
                             ", array(get_the_ID()))
-                            );
-                            if (count($related_campus) > 0) {
-                                foreach ($related_campus as $campus) {
-                                    echo "<a href=$campus->guid>$campus->post_name</a><br>";
+                                );
+                                if (count($related_campus) > 0) {
+                                    foreach ($related_campus as $campus) {
+
+                                        echo "<a href=$campus->guid>$campus->post_title</a><br>";
+                                    }
                                 }
-                            }
-                            ?>
-                        </div>
-                    </article>
-                <?php endwhile; ?>
+                                ?>
+                            </div>
+                        </article>
+                    <?php endwhile; ?>
+                </div>
             </div>
         </section>
 
@@ -166,17 +177,29 @@
                 <header>
                     <h1>Professeurs</h1>
                 </header>
+                <div class="professor-post">
+                    <?php while ($professors->have_posts()) : $professors->the_post(); ?>
+                        <article>
+                            <figure>
+                                <div class="professor-img">
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
+                                <figcaption>
+                                    <h3><?php the_title(); ?></h3>
+                                </figcaption>
+                            </figure>
 
-                <?php while ($professors->have_posts()) : $professors->the_post(); ?>
-                    <article class="professor-post">
-                        <div class="professor-img">
-                            <?php the_post_thumbnail(); ?>
-                        </div>
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php the_excerpt(); ?></p>
-                        <small><?php echo get_post_meta(get_the_ID(), 'email', true) ?></small>
-                    </article>
-                <?php endwhile; ?>
+
+                            <div class="professor-info">
+                                <?php the_excerpt(); ?>
+                                <small>Contact: <?php echo get_post_meta(get_the_ID(), 'email', true) ?></small>
+                            </div>
+
+
+                        </article>
+                    <?php endwhile; ?>
+                </div>
+
             </div>
         </section>
 
