53d508ca3b825d3e9c625bade287cb1058e82d9d
ultimecreation <naserbechata@gmail.com>
2020-02-26 16:58:19 +0100

    search input style completed

--- wp-content/themes/wp-university/searchform.php
+++ wp-content/themes/wp-university/searchform.php
@@ -1,4 +1,5 @@
 <form id="searchform" method="get" action="<?php echo home_url('/'); ?>">
+
     <input type="text" class="search-field" name="s" placeholder="Entrez votre recherche" value="<?php the_search_query(); ?>">
     <input type="hidden" name="post_type[]" value="campus" />
     <input type="hidden" name="post_type[]" value="event" />
