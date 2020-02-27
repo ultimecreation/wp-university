dafacca1b77f6f207dfcb6f4800c675c4fa270af
ultimecreation <naserbechata@gmail.com>
2020-02-27 10:58:44 +0100

    single program completed

---
+++ wp-content/themes/wp-university/partials/content-program.php
@@ -0,0 +1,6 @@
+<header>
+    <h1><?php the_title(); ?></h1>
+</header>
+<article>
+    <?php the_content(); ?>
+</article>
--- wp-content/themes/wp-university/single.php
+++ wp-content/themes/wp-university/single.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 
 <main id="single">
-    <header>
-        <h1>Single</h1>
-    </header>
+    <div class="container">
+        <?php if (have_posts()) : the_post(); ?>
+            <?php get_template_part('partials/content', get_post_type()); ?>
+
+        <?php endif; ?>
+    </div>
+
 </main>
 <?php get_footer(); ?>
